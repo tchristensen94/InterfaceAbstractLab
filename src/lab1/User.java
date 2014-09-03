5c65b95d16d9fd832bb735e4fafa76b2eaf17917
rustedarrows <ch4nger@gmail.com>
2014-09-03 13:25:42 -0500

    Main class

---
+++ src/lab1/User.java
@@ -0,0 +1,38 @@
+package lab1;
+
+import lab1.Course;
+
+import java.util.LinkedList;
+
+/**
+ * Created by Timothy on 9/3/2014.
+ */
+public class User {
+    private String name;
+    private double credits;
+    private LinkedList<Course> enrolled = new LinkedList<Course>();
+    private LinkedList<Course> previouslyEnrolled = new LinkedList<Course>();
+
+
+    public User(String name, double credits) {
+        this.name = name;
+        this.credits = credits;
+    }
+
+    public void enroll(Course course) {
+        enrolled.add(course);
+    }
+
+    public boolean hasEnrolledPreviously(Course course) {
+        return previouslyEnrolled.contains(course);
+    }
+    public LinkedList<Course> currentlyEnrolled() {
+        return enrolled;
+    }
+    public void addPreviousEnrolled(Course course) {
+        previouslyEnrolled.add(course);
+    }
+    public String getName() {
+        return name;
+    }
+}
